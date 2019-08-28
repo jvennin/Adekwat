789012a92debde488d24ae0cd843444a6b4e59c9
Aurelie Mahe <mahe.aureliems@gmail.com>
2019-08-28 19:05:49 +0200

    style slider

---
+++ app/javascript/components/slide.js
@@ -0,0 +1,12 @@
+const slider = () => {
+  const slider = document.getElementById("walkingDurationId");
+  const output = document.getElementById("steps");
+  output.innerHTML = slider.value;
+
+  slider.oninput = function() {
+    output.innerHTML = this.value;
+  };
+};
+
+export { slider }
+
