9490d08b09aefb3dc0ae1d049f7b60a467dcbfd7
Julien VENNIN <julien.vennin@gmail.com>
2019-08-29 12:18:10 +0200

    add mapbox on itinerary controller + design fake card

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,13 @@
 import "bootstrap";
+import 'mapbox-gl/dist/mapbox-gl.css';
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
 import { initAutocomplete } from "../plugins/init_autocomplete";
 import { slider } from "../components/slide";
+import { initMapbox } from '../plugins/init_mapbox';
 
 initAutocomplete();
+initMapbox();
 
 if (document.getElementById('walkingDurationID')) {
   slider();
