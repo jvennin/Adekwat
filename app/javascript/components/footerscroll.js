97eba16f97228626fedb235bbf574a958e04f5f7
Julien VENNIN <julien.vennin@gmail.com>
2019-08-30 15:51:12 +0200

    Correction design et js

--- app/javascript/components/footerscroll.js
+++ app/javascript/components/footerscroll.js
@@ -3,28 +3,40 @@ const footerscroll = () => {
   const ones = document.querySelectorAll('#foot-one');
   ones.forEach((one) => {
     one.addEventListener('click', () => {
-      document.querySelector('#ans-one').scrollIntoView({ block: 'start', behavior: 'smooth' });
+      console.log("clic on 1");
+      document.querySelector('#ans-top-one').scrollIntoView({ block: 'start', behavior: 'smooth' });
     })
   });
 
   const twos = document.querySelectorAll('#foot-two');
   twos.forEach((two) => {
     two.addEventListener('click', () => {
-      document.querySelector('#ans-two').scrollIntoView({ block: 'start', behavior: 'smooth' });
+      console.log("clic on 2");
+      document.querySelector('#ans-top-two').scrollIntoView({ block: 'start', behavior: 'smooth' });
     })
   });
 
   const threes = document.querySelectorAll('#foot-three');
   threes.forEach((three) => {
     three.addEventListener('click', () => {
-      document.querySelector('#ans-three').scrollIntoView({ block: 'start', behavior: 'smooth' });
+      console.log("clic on 3");
+      document.querySelector('#ans-top-three').scrollIntoView({ block: 'start', behavior: 'smooth' });
     })
   });
 
   const fours = document.querySelectorAll('#foot-four');
   fours.forEach((four) => {
     four.addEventListener('click', () => {
-      document.querySelector('#ans-four').scrollIntoView({ block: 'start', behavior: 'smooth' });
+      console.log("clic on 4");
+      document.querySelector('#ans-top-four').scrollIntoView({ block: 'start', behavior: 'smooth' });
+    })
+  });
+
+  const fives = document.querySelectorAll('#foot-five');
+  fives.forEach((five) => {
+    five.addEventListener('click', () => {
+      console.log("clic on 5");
+      document.querySelector('##ans-top-five').scrollIntoView({ block: 'end', behavior: 'smooth' });
     })
   });
 
