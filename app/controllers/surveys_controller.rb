c324c529fea792e013989c9166e8a71225162f67
Aurelie Mahe <mahe.aureliems@gmail.com>
2019-09-03 18:37:14 +0200

    create survey actions

---
+++ app/controllers/surveys_controller.rb
@@ -0,0 +1,21 @@
+class SurveysController < ApplicationController
+  skip_after_action :verify_authorized
+
+  def answer
+    if params[:poi] == "true"
+      poi = Poi.find(params[:poi_id])
+      poi.update(escalator: params[:escalator])
+    end
+
+    profile = current_user.profile
+    score = profile.score.to_i
+    score += 5
+    profile.score = score
+    profile.save
+    redirect_to profile_path(profile)
+  end
+
+  def form
+    @poi = Poi.find(params[:poi_id])
+  end
+end
