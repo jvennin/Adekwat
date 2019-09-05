b7a2aaac692495df84d15b127fb4501a0a4f6785
caroline debeve <c.debeve@gmail.com>
2019-09-05 17:32:45 +0200

    active and scroll

--- app/javascript/components/autoscroll.js
+++ app/javascript/components/autoscroll.js
@@ -1,6 +1,6 @@
 const autoscroll = () => {
-
   const ones = document.querySelectorAll('#ans-one');
+
   ones.forEach((one) => {
     one.addEventListener('click', () => {
       document.querySelector('.two').scrollIntoView({ block: 'start', behavior: 'smooth' });
@@ -11,6 +11,7 @@ const autoscroll = () => {
   twos.forEach((two) => {
     two.addEventListener('click', () => {
       document.querySelector('.three').scrollIntoView({ block: 'start', behavior: 'smooth' });
+      document.querySelector('#foot-two img').src = '/assets/person-walking3.png'
     })
   });
 
@@ -18,6 +19,7 @@ const autoscroll = () => {
   threes.forEach((three) => {
     three.addEventListener('click', () => {
       document.querySelector('.four').scrollIntoView({ block: 'start', behavior: 'smooth' });
+      document.querySelector('#foot-three img').src = '/assets/ascending-stairs-signal2.png'
     })
   });
 
@@ -25,6 +27,7 @@ const autoscroll = () => {
   fours.forEach((four) => {
     four.addEventListener('click', () => {
       document.querySelector('.five').scrollIntoView({ block: 'start', behavior: 'smooth' });
+      document.querySelector('#foot-four img').src = '/assets/business-affiliate-network3.png'
     })
   });
 
